c0da2958339ee48dc79bc258c26aa524bdafd15c
AntonioFlores1 <af513249@gmail.com>
2019-07-25 15:15:14 -0400

    Created tracker to place arena and players, Added textures and added floor to file

--- ZombieWaveAR/ViewController.swift
+++ ZombieWaveAR/ViewController.swift
@@ -9,54 +9,103 @@
 import UIKit
 import SceneKit
 import ARKit
+import SpriteKit
+import AVFoundation
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController, ARSCNViewDelegate,ARSessionDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var trackerNode: SCNNode!
+    var Gamestarted = false
+    var foundaSurface = false
+    var trackingForArenaPosition = SCNVector3Make(0.0, 0.0, 0.0)
+    
+    
+   var arenaNode: SCNNode!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Set the view's delegate
-        sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
+//        constraints()
+let scene = SCNScene(named: "art.scnassets/CityStreet/untitled.dae")!
         sceneView.scene = scene
+        sceneView.delegate = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        // Create a session configuration
+        sceneView.session.delegate = self
         let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
         sceneView.session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
-        // Pause the view's session
         sceneView.session.pause()
     }
 
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+        
+        if Gamestarted {
+            
+        } else {
+            
+            guard let safeUnwrap = trackerNode else {return}
+            
+            safeUnwrap.removeFromParentNode()
+            
+            Gamestarted = true
+            
+            guard let arena = sceneView.scene.rootNode.childNode(withName: "rua", recursively: false) else {return}
+            
+            arenaNode = arena
+            
+            arenaNode.position = SCNVector3Make(trackingForArenaPosition.x, trackingForArenaPosition.y, trackingForArenaPosition.z)
+            
+//            arenaNode.scale = SCNVector3(0.025,0.025,0.025)
+            sceneView.scene.rootNode.addChildNode(arenaNode)
+            arenaNode.isHidden = false
+        }
     }
-*/
+    
+    
+    
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        guard !Gamestarted else {return}
+        
+        guard let hitTest = sceneView.hitTest(CGPoint(x: view.frame.midX, y: view.frame.midX), types: [.existingPlane,.featurePoint,.estimatedHorizontalPlane]).first else {return}
+        
+        let trans = SCNMatrix4(hitTest.worldTransform)
+        trackingForArenaPosition = SCNVector3Make(trans.m41, trans.m42, trans.m43)
+        
+        if !foundaSurface {
+            let trackerPlane = SCNPlane(width: 0.15, height: 0.15)
+            trackerPlane.firstMaterial?.diffuse.contents = UIImage.init(named: "placer")
+            trackerPlane.firstMaterial?.isDoubleSided =  true
+            trackerNode = SCNNode(geometry: trackerPlane)
+            trackerNode.eulerAngles.x = -.pi * 0.5
+            sceneView.scene.rootNode.addChildNode(trackerNode)
+            foundaSurface = true
+        }
+        
+        trackerNode.position = trackingForArenaPosition
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
